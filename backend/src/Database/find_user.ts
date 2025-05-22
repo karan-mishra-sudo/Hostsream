87ae73e770287f2fc8261356a85ef1aec89d8f13
karan-mishra-sudo <mishrazack69@gmail.com>
2025-05-22 20:39:58 +0530

    Delete backend directory

--- backend/src/Database/find_user.ts
+++
@@ -1,6 +0,0 @@
-import {UserModel} from "./model.js";
-import { user_type } from "../Data/types.js";
-
-export default async function find_user(user: user_type) {
- return await UserModel.findOne({id:user.id});
-}
--- backend/src/Middleware/index.ts
+++
@@ -1,4 +0,0 @@
-import get_site_route from "./get_site_route.js";
-export default {
-    get_site_route
-}
